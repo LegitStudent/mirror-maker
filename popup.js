f243ac5f492d45b81af1a5b077271529b6c4e8e3
Jasper Francisco <jasper.francisco@gmail.com>
2017-06-20 15:44:12 +0800

    Rewrite writeStatusMessage function
    It now appends a link for successfully uploaded videos. It also verifies shortcodes, make sure users don't get XSS'd.

--- popup.js
+++ popup.js
@@ -2,15 +2,34 @@ var streamableUrl = "https://api.streamable.com/import";
 
 function uploadVideoToStreamable(url, title = "") {
   $.get(streamableUrl, { url, title }, function(response, status) {
-    writeStatusMessage("Success! You may view your video at https://streamable.com/" +  response.shortcode);
+    writeStatusMessage("Success! You may view your video at ", response.shortcode);
   }, "json")
   .fail(function(jqXHR, status, error) {
     alert(error);
   });
 }
 
-function writeStatusMessage(message) {
-  $(".upload-form > .status").text(message);
+function writeStatusMessage(message, shortcode="") {
+  
+
+  $(".upload-form > .status").text(message)
+
+  /*
+    For a success status, a valid shortcode should exist.
+    Append this valid shortcode to the message for easy access.
+
+    Validation of shortcode is based on string length. This will
+    break when Streamable changes its shortcode format.
+  */
+  .append(function(index, oldHtml) {
+    var videoUrl = "https://streamable.com/" + shortcode;
+    if (shortcode.toString().length === 5) {
+      return "<a target='_blank' href='" + videoUrl + "'>" + videoUrl + "</a>";
+    } else {
+      return false;
+    }
+  });
+
 }
 
 $(document).ready(function() {
